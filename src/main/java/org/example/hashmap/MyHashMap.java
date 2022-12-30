cae9ac1c8bf67c8de31555737aad357a46b50523
this-is-spear <rjsckdd12@gmail.com>
2022-12-30 14:56:25 +0900

    Init : 초기 환경 구성

---
+++ src/main/java/org/example/hashmap/MyHashMap.java
@@ -0,0 +1,20 @@
+package org.example.hashmap;
+
+public class MyHashMap {
+
+  public MyHashMap() {
+
+  }
+
+  public void put(int key, int value) {
+
+  }
+
+  public int get(int key) {
+    return 0;
+  }
+
+  public void remove(int key) {
+
+  }
+}
